6f892e2434e9502672b699f2c39ee25de9237819
Elizabth-Zelaya-R <elizabeth.zelaya@unah.hn>
2025-03-18 21:51:31 -0600

    Modificacion de migraciones

--- database/migrations/2025_03_17_055413_create_stores_table.php
+++ database/migrations/2025_03_17_055413_create_stores_table.php
@@ -6,21 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+
+    public function up()
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('url');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('stores');
     }
