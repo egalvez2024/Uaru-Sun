23984c153a5d02173cf01f8c5a0f133b515e8ee5
FelipeJ12 <felipe.jimenez@unah.hn>
2025-04-01 14:16:49 -0600

    añadi categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,34 +15,15 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Categoria::create([
-            'nombre' => 'Arboles',
-            'tipo' => 'flora'
+            'nombre' => 'Mamiferos',
+            'tipo' => 'fauna'   
            
         ]);
-        Categoria::create([
-            'nombre' => 'Fauna',
-            'tipo' => 'Animal'
-           
-        ]);
-        Categoria::create([
-            'nombre' => 'Flora',
-            'tipo' => 'Planta'
-        ]);
-        Categoria::create([
-            'nombre' => 'Peligro de Extincion',
-            'tipo' => 'Peligro'
-        ]);
-        Categoria::create([
-            'nombre' => 'Anfibios',
-            'tipo' => 'reptil'
-           
-        ]);
-        Categoria::create([
-            'nombre' => 'Paisajes',
-            'tipo' => ''
-        ]);
+       
     
-
+       
+    
+        
 
     }
 }
