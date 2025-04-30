32492dac7534332bd248f69b7ae47b586cdf8179
FelipeJ12 <felipe.jimenez@unah.hn>
2025-04-29 19:29:28 -0600

    actualizacion del guardado

--- app/Http/Controllers/UsuarioPostController.php
+++ app/Http/Controllers/UsuarioPostController.php
@@ -8,7 +8,6 @@ use App\Models\Categoria;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Auth;
 
 class UsuarioPostController extends Controller
 {
